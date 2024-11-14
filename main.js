b216e12f2b89ef998d18cde7a1d6cff6cd1b312a
GHURTSKAIA GIORGI <ghurtska@gmail.com>
2024-11-14 16:59:11 +0400

    #1

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log(17 + 17);
+
+console.log(20 * 20);
+
+console.log("Hello name" + " " + "giorgi");
+
+console.log(" g h u r t s k a i a ".toLocaleUpperCase());
+
+console.log(" G I O R G I ".toLowerCase());
